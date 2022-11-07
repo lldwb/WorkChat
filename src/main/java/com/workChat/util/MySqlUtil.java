86c5e4a43ddfc4a0bf0a66ab4f770126826f749a
lldwb <3247187440@qq.com>
2022-11-07 22:42:05 +0800

    Initial commit
    Signed-off-by: lldwb <3247187440@qq.com>

--- src/main/java/com/workChat/util/MySqlUtil.java
+++ src/main/java/com/workChat/util/MySqlUtil.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class MySqlUtil {
     //数据库连接参数
     //数据库地址
-    private static final String URL = "jdbc:mysql://mysql.lldwb.top:33366/register?serverTimezone=Asia/Shanghai";
+    private static final String URL = "jdbc:mysql://mysql.lldwb.top:33366/workchatsql?serverTimezone=Asia/Shanghai";
     //账号
     private static final String USER_NAME = "root";
     //密码
