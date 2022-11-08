6ef50f4cccf689dba0cbb25198d7c1718b8bad43
lldwb <3247187440@qq.com>
2022-11-08 11:35:17 +0800

    Initial commit
    Signed-off-by: lldwb <3247187440@qq.com>

--- src/main/java/com/workChat/entity/GroupChat.java
+++ src/main/java/com/workChat/entity/GroupChat.java
@@ -1,5 +1,6 @@
 package com.workChat.entity;
 
+
 import java.sql.Timestamp;
 
 /**
@@ -12,4 +13,44 @@ public class GroupChat {
     private int unitId;
     private Timestamp time;
     private String word;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getUnitId() {
+        return unitId;
+    }
+
+    public void setUnitId(int unitId) {
+        this.unitId = unitId;
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
+
+    public void setTime(Timestamp time) {
+        this.time = time;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
 }
