dc0581486fffcb891c4c6d211ae2a053ddaa8dd6
hwd <3274865074@qq.com>
2022-11-08 20:31:30 +0800

    实现类

--- src/main/java/com/workChat/dao/SqlDaoImpl.java
+++ src/main/java/com/workChat/dao/SqlDaoImpl.java
@@ -6,12 +6,14 @@ import com.workChat.util.MySqlUtil;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author 谢世杰
  * @version 1.0
  */
+@SuppressWarnings({"all"})
 public class SqlDaoImpl implements SqlDao {
     @Override
     public int addUnitIdUser(String name,String pwd,int unitId) {
@@ -21,7 +23,24 @@ public class SqlDaoImpl implements SqlDao {
 
     @Override
     public List<GroupChat> getUnitIdGroupChat(int unitId) {
-        return null;
+
+        try {
+            ResultSet rs = MySqlUtil.queryResultSet("select * from groupChat where id=?", unitId);
+            List<GroupChat> groupChatList = new ArrayList<>();
+            while (rs.next()){
+                GroupChat groupChat = new GroupChat();
+                groupChat.setId(rs.getInt("id"));
+                groupChat.setUserId(rs.getInt("userId"));
+                groupChat.setUnitId(rs.getInt("unitId"));
+                groupChat.setTime(rs.getTimestamp("time"));
+                groupChat.setWord(rs.getString("word"));
+                groupChatList.add(groupChat);
+            }
+            return groupChatList;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
@@ -47,6 +66,21 @@ public class SqlDaoImpl implements SqlDao {
 
     @Override
     public List<User> getUnitIdUser(int unitId) {
-        return null;
+        try {
+            ResultSet rs = MySqlUtil.queryResultSet("select * from user where id=?", unitId);
+            List<User> userList = new ArrayList<>();
+            while (rs.next()){
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setPwd(rs.getString("pwd"));
+                user.setUnitId(rs.getInt("unitId"));
+                user.setRemark(rs.getString("remark"));
+            }
+            return userList;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
