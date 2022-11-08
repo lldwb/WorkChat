2389d5a6bddc831090db836531c405eb2cbc8472
lldwb <3247187440@qq.com>
2022-11-08 11:34:15 +0800

    Initial commit
    Signed-off-by: lldwb <3247187440@qq.com>

---
+++ src/main/java/com/workChat/entity/GroupChat.java
@@ -0,0 +1,15 @@
+package com.workChat.entity;
+
+import java.sql.Timestamp;
+
+/**
+ * @author 安然的尾巴
+ * @version 1.0
+ */
+public class GroupChat {
+    private int id;
+    private int userId;
+    private int unitId;
+    private Timestamp time;
+    private String word;
+}
