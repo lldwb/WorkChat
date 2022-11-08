02bc15fedd597a407fada3a67dd857798351ae59
lldwb <3247187440@qq.com>
2022-11-08 16:46:05 +0800

    Initial commit
    Signed-off-by: lldwb <3247187440@qq.com>

--- src/main/java/com/workChat/service/WorkChat.java
+++ src/main/java/com/workChat/service/WorkChat.java
@@ -14,7 +14,7 @@ public interface WorkChat {
     int addUnitIdUser(int unitId);
     //根据id返回用户
     User getIdUser(int id);
-    //根据单位编号返回用户集合
+    //根据单位编号返回用户集合 黄为东
     List<User> getUnitIdUser(int unitId);
 
     //通过单位编号返回GroupChat集合
