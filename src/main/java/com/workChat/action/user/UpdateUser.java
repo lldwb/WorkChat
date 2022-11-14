42d673c3e92d8398a632c8b6213737a97ce79c31
TP9527 <215168884@qq.com>
2022-11-14 19:35:14 +0800

    Initial commit
    Signed-off-by: lldwb <3247187440@qq.com>
    Signed-off-by: TP9527 <215168884@qq.com>

--- src/main/java/com/workChat/action/user/UpdateUser.java
+++ src/main/java/com/workChat/action/user/UpdateUser.java
@@ -11,8 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/updateUser")
-public class updateUser extends HttpServlet {
+@WebServlet("/UpdateUser")
+public class UpdateUser extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("utf-8");
--- src/main/java/com/workChat/dao/SqlDaoImpl.java
+++ src/main/java/com/workChat/dao/SqlDaoImpl.java
@@ -21,7 +21,7 @@ public class SqlDaoImpl implements SqlDao {
 
     @Override
     public int updateUser(int id,String name, String pwd) {
-        return mySqlUtil.update("update updateUser sel id=?, name=?, pwd=?",id,name,pwd);
+        return mySqlUtil.update("update UpdateUser sel name=?, pwd=? where id=?",name,pwd,id);
     }
 
     @Override
