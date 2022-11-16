1c4b97926893ea533c63df4d15d8420a9bf80a4a
tianyuanlizian <1572665364@qq.com>
2022-11-16 15:28:35 +0800

    Initial commit
    Signed-off-by: lldwb <3247187440@qq.com>
    Signed-off-by: tianyuanlizian <1572665364@qq.com>

--- src/main/webapp/js/c.js
+++ src/main/webapp/js/c.js
@@ -1,8 +1,8 @@
 Vue.component("myc",{
     data(){
         return{
-            listw:this.$route.params.listw,
-            sj:this.$route.params.sj,
+            listw:that.$route.params.listw,
+            sj:that.$route.params.sj,
         }
 
     },
