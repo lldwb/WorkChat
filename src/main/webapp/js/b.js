b9896700087191293895695d8bbb1d18a8915cb9
tianyuanlizian <1572665364@qq.com>
2022-11-16 16:11:44 +0800

    Initial commit
    Signed-off-by: lldwb <3247187440@qq.com>
    Signed-off-by: tianyuanlizian <1572665364@qq.com>

--- src/main/webapp/js/b.js
+++ src/main/webapp/js/b.js
@@ -5,7 +5,7 @@ Vue.component("myb",{
     },
     template : "<div style=\"background-color:#F5F5F5;width: 100%;height: 540px\">" +
         "<van-row type=\"flex\">\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"单格\" size=\"large\" is-link /></van-col>\n" +
+        "        <van-col span=\"24\" ><van-cell title=\"单元格\" size=\"large\" is-link /></van-col>\n" +
         "        <van-col span=\"24\" ><van-cell title=\"单元格\" size=\"large\" is-link /></van-col>\n" +
         "        <van-col span=\"24\" ><van-cell title=\"单元格\" size=\"large\" is-link /></van-col>\n" +
         "        <van-col span=\"24\" ><van-cell title=\"单元格\" size=\"large\" is-link /></van-col>\n" +
