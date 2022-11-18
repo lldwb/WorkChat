3b0eb6e59d52b7c144768d4d639c8713aff9143c
tianyuanlizian <1572665364@qq.com>
2022-11-18 10:47:58 +0800

    Initial commit
    Signed-off-by: lldwb <3247187440@qq.com>
    Signed-off-by: tianyuanlizian <1572665364@qq.com>

--- src/main/webapp/js/c.js
+++ src/main/webapp/js/c.js
@@ -10,7 +10,7 @@ Vue.component("myc",{
               <van-row type="flex" style="background-color:#F5F5F5;padding-bottom: 60px">
               <van-index-bar :index-list=listw>
               <van-col span="24" ><van-cell title="联系人" /></van-col>
-              <van-col span="24" ><van-index-anchor index="1">{{sj.name}}</van-index-anchor></van-col>
+              <van-col span="24" ><van-index-anchor index="1">{{sju.name}}</van-index-anchor></van-col>
             </van-index-bar>
             </van-row>
              </div>`
