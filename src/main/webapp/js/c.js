faa35202653ec5bc52bf65fd9c0af98e9cb45859
tianyuanlizian <1572665364@qq.com>
2022-11-18 11:39:03 +0800

    Initial commit
    Signed-off-by: lldwb <3247187440@qq.com>
    Signed-off-by: tianyuanlizian <1572665364@qq.com>

--- src/main/webapp/js/c.js
+++ src/main/webapp/js/c.js
@@ -6,34 +6,18 @@ Vue.component("myc",{
         }
 
     },
-    template :"<van-row type=\"flex\" style=\"background-color:#F5F5F5;padding-bottom: 60px\">\n" +
-        "        <van-index-bar :index-list=listw>\n" +
-        "\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"联系人\" /></van-col>\n" +
-        "        <van-col span=\"24\" ><van-index-anchor index=\"1\">{{sj}}</van-index-anchor></van-col>\n" +
-        "        <van-col span=\"24\" ><van-cell v-model='sj' /></van-col>\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"杰2\" /></van-col>\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"杰3\" /></van-col>\n" +
-        "\n" +
-        "        <van-col span=\"24\" ><van-index-anchor index=\"2\">杰</van-index-anchor></van-col>\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"杰1\" /></van-col>\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"杰2\" /></van-col>\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"杰3\" /></van-col>\n" +
-        "\n" +
-        "        <van-col span=\"24\" ><van-index-anchor index=\"3\">杰</van-index-anchor></van-col>\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"杰1\" /></van-col>\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"杰2\" /></van-col>\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"杰3\" /></van-col>\n" +
-        "\n" +
-        "        <van-col span=\"24\" ><van-index-anchor index=\"4\">杰</van-index-anchor></van-col>\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"杰1\" /></van-col>\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"杰2\" /></van-col>\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"杰3\" /></van-col>\n" +
-        "\n" +
-        "        <van-col span=\"24\" ><van-index-anchor index=\"5\">杰</van-index-anchor></van-col>\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"杰1\" /></van-col>\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"杰2\" /></van-col>\n" +
-        "        <van-col span=\"24\" ><van-cell title=\"杰3\" /></van-col>\n" +
-        "    </van-index-bar>\n" +
-        "    </van-row>\n"
+    template :`<van-row type="flex" style="background-color:#F5F5F5;padding-bottom: 60px">
+                 <van-index-bar :index-list="listw">
+
+          <van-col span="24" ><van-cell title="联系人" /></van-col>
+        <van-col span="24" ><van-index-anchor index="1">杰</van-index-anchor></van-col>
+        <div v-for="shu in sj">
+        <van-col span="24" ><van-cel />{{shu.name}}</van-col>
+        <van-col span="24" ><van-cell title="杰2" /></van-col>
+        <van-col span="24" ><van-cell title="杰3" /></van-col>
+        </div>
+    </van-index-bar>
+    </van-row>
+`
+
 })
