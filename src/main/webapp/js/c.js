de29c9f10ecb2035bec5462e050fc9c40d32166f
tianyuanlizian <1572665364@qq.com>
2022-11-18 11:49:54 +0800

    Initial commit
    Signed-off-by: lldwb <3247187440@qq.com>
    Signed-off-by: tianyuanlizian <1572665364@qq.com>

--- src/main/webapp/js/c.js
+++ src/main/webapp/js/c.js
@@ -11,11 +11,14 @@ Vue.component("myc",{
 
           <van-col span="24" ><van-cell title="联系人" /></van-col>
         <van-col span="24" ><van-index-anchor index="1">杰</van-index-anchor></van-col>
+        
+        <van-col span="24" >
         <div v-for="shu in sj">
         <van-col span="24" ><van-cel />{{shu.name}}</van-col>
         <van-col span="24" ><van-cell title="杰2" /></van-col>
         <van-col span="24" ><van-cell title="杰3" /></van-col>
         </div>
+        </van-col>
     </van-index-bar>
     </van-row>
 `
