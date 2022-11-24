9327dfdce1090a0a82947bc89fc1828c7b400886
tianyuanlizian <1572665364@qq.com>
2022-11-24 20:40:40 +0800

    Initial commit
    Signed-off-by: lldwb <3247187440@qq.com>
    Signed-off-by: tianyuanlizian <1572665364@qq.com>

--- src/main/webapp/js/b.js
+++ src/main/webapp/js/b.js
@@ -1,36 +1,17 @@
 Vue.component("myb",{
     data(){
         return {
+            sj:this.$route.params.sj,
         }
     },
-    template : "<div style=\"background-color:#F5F5F5;width: 100%;height: 540px\">" +
-        "<van-row style=\"background-color: white;margin-bottom: 5px\">\n" +
-        "        <van-col span=\"7\" style=\"height: 70px; text-align: center;margin-top: 10px\">\n" +
-        "            <img src=\"https://img01.yzcdn.cn/vant/cat.jpeg\" width=\"50px\" height=\"50px\"  />\n" +
-        "        </van-col>\n" +
-        "        <van-col span=\"17\" style=\"height: 70px\">\n" +
-        "            <van-cell title=\"小新同学\" url=\"chat.html\">\n" +
-        "            </van-cell>\n" +
-        "        </van-col>\n" +
-        "    </van-row >\n" +
-        "    <van-row style=\"background-color: white;margin-bottom: 5px\">\n" +
-        "        <van-col span=\"7\" style=\"height: 70px; text-align: center;margin-top: 10px\">\n" +
-        "            <img src=\"img/xw.jpg\" width=\"50px\" height=\"50px\"  />\n" +
-        "        </van-col>\n" +
-        "        <van-col span=\"17\" style=\"height: 70px\">\n" +
-        "            <van-cell title=\"小威同学\" url=\"chat2.html\">\n" +
-        "            </van-cell>\n" +
-        "        </van-col>\n" +
-        "    </van-row >\n" +
-        "    <van-row style=\"background-color: white;margin-bottom: 5px\">\n" +
-        "        <van-col span=\"7\" style=\"height: 70px; text-align: center;margin-top: 10px\">\n" +
-        "            <img src=\"img/xl.jpg\" width=\"50px\" height=\"50px\"  />\n" +
-        "        </van-col>\n" +
-        "        <van-col span=\"17\" style=\"height: 70px\">\n" +
-        "            <van-cell title=\"小林同学\" url=\"chat3.html\">\n" +
-        "            </van-cell>\n" +
-        "        </van-col>\n" +
-        "    </van-row >" +
-
-        "</div>"
+    template : `<div style="background-color:#F5F5F5;width: 100%;height: 540px">
+        <van-row type="flex" style="background-color: white;margin-bottom: 5px" v-for="shu in sj">
+                <van-col span="7" style="height: 70px; text-align: center;margin-top: 10px">
+                    <img src="https://img01.yzcdn.cn/vant/cat.jpeg" width="50px" height="50px"  />
+                </van-col>
+               <van-col span="17" style="height: 70px;" >
+                    <van-notice-bar style="margin-top: 5%" color="black" background="white">{{shu.name}}</van-notice-bar>
+                </van-col>
+           </van-row >
+        </div\>`
 })
