a6a99f102f7813bd0cbca3b56c22a3abf4b0cbeb
tianyuanlizian <1572665364@qq.com>
2022-11-27 20:41:22 +0800

    Initial commit
    Signed-off-by: lldwb <3247187440@qq.com>
    Signed-off-by: tianyuanlizian <1572665364@qq.com>

--- src/main/webapp/js/b.js
+++ src/main/webapp/js/b.js
@@ -4,13 +4,19 @@ Vue.component("myb",{
             sj:this.$route.params.sj,
         }
     },
+    methods:{
+        ss (i){
+            sj:this.$route.params.sj,
+                location.href = 'chat.html?receiveId='+sj[i].id;
+        }
+    },
     template : `<div style="background-color:#F5F5F5;width: 100%;height: 540px">
-        <van-row type="flex" style="background-color: white;margin-bottom: 5px" v-for="shu in sj">
+        <van-row type="flex" style="background-color: white;margin-bottom: 5px" v-for="(shu,i) in sj">
                 <van-col span="7" style="height: 70px; text-align: center;margin-top: 10px">
                     <img src="https://img01.yzcdn.cn/vant/cat.jpeg" width="50px" height="50px"  />
                 </van-col>
                <van-col span="17" style="height: 70px;" >
-                    <van-cell style="margin-top: 5%" url="chat.html?receiveId=1">{{shu.name}}</van-cell>
+                    <van-cell style="margin-top: 5%" @click="ss(i)">{{shu.name}}</van-cell>
                 </van-col>
            </van-row >
         </div\>`
--- src/main/webapp/js/c.js
+++ src/main/webapp/js/c.js
@@ -6,13 +6,19 @@ Vue.component("myc",{
         }
 
     },
+    methods:{
+        ss (i){
+            sj:this.$route.params.sj,
+            location.href = 'chat.html?receiveId='+sj[i].id;
+        }
+    },
     template :`
            <van-index-bar :index-list="listw">
            <van-row type="flex" style="background-color: #F5F5F5;">
           <van-col span="24" ><van-cell title="联系人" style="width: 100%;height: 100%"/></van-col>
         <van-col span="24" ><van-index-anchor index="1">A组</van-index-anchor></van-col>
-        <van-col span="24" v-for="shu in sj">
-        <van-cell url="chat.html?receiveId=1"background="white">{{shu.name}}</van-cell>
+        <van-col span="24" v-for="(shu,i) in sj">
+        <van-cell @click="ss(i)" background="white">{{shu.name}}</van-cell>
         </van-col> 
          </van-row>
     </van-index-bar>
